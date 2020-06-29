a143f5a53c7b91b2d7da3f0de75eeab9686bdf80
chakanish <nil.chakravartty@outlook.com>
2020-06-29 16:52:57 +0530

    Dev changes ::Anish

--- src/components/Settings/FbConnect.vue
+++ src/components/Settings/FbConnect.vue
@@ -182,13 +182,6 @@ export default {
     step1Comp() {
       Vue.FB.getLoginStatus(function(response) {
         console.log("FBAUTH status :: " + response.status);
-        console.log(
-          "User Id :: " + this.$store.getters.getSettingsState.facebook_user_id
-        );
-        console.log(
-          "Access Token ::" +
-            this.$store.getters.getSettingsState.facebook_short_access_token
-        );
 
         if (response.status == "connected") {
           let fbUsrId = response.authResponse.userID;
