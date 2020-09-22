9cb62aaa2218078180383e5b1bb1c200c5b5816f
chakanish <nil.chakravartty@outlook.com>
2020-09-22 21:36:12 +0530

    Cloudbuild test :: Anish

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, ));
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
 const port = process.env.PORT || 8080;
