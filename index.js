19a5bb3814b5286ff486dfac01ec4d09b8ebb98a
chakanish <nil.chakravartty@outlook.com>
2020-09-22 22:00:27 +0530

    Cloudbuild test :: Anish

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.use(cors);
 //here we are configuring dist to serve app files
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
+
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
+  // res.sendFile(path.join(__dirname, "/dist/index.html"));
+  res.sendFile('/dist/index.html', { root: __dirname })
 });
 
 const port = process.env.PORT || 8080;
